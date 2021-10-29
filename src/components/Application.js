177144843fdec219d323d0133837b7f601a4904f
Saam Koukpari <saam.koukpari@gmail.com>
2021-10-29 14:00:10 -0700

    added api/interviewers

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,29 +5,40 @@ import "components/Application.scss";
 import DayList from "./DayList";
 import InterviewerList from "./InterviewerList";
 import Appointment from "components/Appointment/index.js";
-import { getAppointmentsForDay } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
 export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
     appointments: {},
+    interviewers: {},
   });
  
 
   useEffect(() => {
     Promise.all([
       axios.get("/api/days"),
-      axios.get("/api/appointments")
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers")
     ]).then(all => {
       const days = all[0].data;
       const appointments = all[1].data; 
-      setState(prev => ({...prev, days, appointments }));
+      const interviewers = all[2].data; 
+      setState(prev => ({...prev, days, appointments, interviewers }));
     })
   }, [])
 
   const setDay = day => setState({ ...state, day });
   const dailyAppointments = getAppointmentsForDay(state, state.day);
+  const schedule = dailyAppointments.map(appointment => {
+    const interview = getInterview(state, appointment.interview);
+    return (
+    <Appointment key={appointment.id} {...appointment}/>
+    );
+  });
+  
+  console.log(state.interviewers)
 
   return (
     <main className="layout">
@@ -59,11 +70,7 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {dailyAppointments.map(appointment => {
-          return (
-        <Appointment key={appointment.id} {...appointment}/>
-        )}
-      )}
+        {schedule}
         <Appointment key="last" time="5pm"/> 
       </section>
     </main>
