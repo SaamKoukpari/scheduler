a2bf7f01d83505b8a52393e7eb1b913280b74c5a
Saam Koukpari <saam.koukpari@gmail.com>
2021-10-29 13:59:33 -0700

    impelement interviewselector function with tests

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -14,3 +14,21 @@ export function getAppointmentsForDay(state, day) {
   }
   return filteredApts; 
 }
+
+export function getInterview(state, interview) {
+  const interviewObj = { student: null, interviewer: null };
+  
+  if (interview) {
+    interviewObj.student = interview.student;
+    for (const interviewer in state.interviewers) {
+      if (state.interviewers[interviewer].id === interview.interviewer) {
+        interviewObj.interviewer = state.interviewers[interviewer];
+      }
+    }
+  }
+  if (interviewObj.student) {
+    return interviewObj;
+  }
+  return null;
+}
+
