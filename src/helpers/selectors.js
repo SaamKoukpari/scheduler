affc0708a58ad9e15fa44d9e6bd7b5c83045eb21
Saam Koukpari <saam.koukpari@gmail.com>
2021-10-31 16:32:37 -0700

    fixed bug

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -13,14 +13,19 @@ export function getAppointmentsForDay(state, dayName) {
 }
 
 export function getInterview(state, interview) {
-  if (!interview) {
-    return null;
+  const resultObj = { student: null, interviewer: null };
+  if (interview) {
+    resultObj.student = interview.student;
+    for (const interviewer in state.interviewers) {
+      if (state.interviewers[interviewer].id === interview.interviewer) {
+        resultObj.interviewer = state.interviewers[interviewer];
+      }
+    }
   }
-
-  return {
-    ...interview,
-    interviewer: state.interviewers[interview.interviewer],
-  };
+  if (resultObj.student) {
+    return resultObj;
+  }
+  return null;
 }
 
 export function getInterviewersForDay(state, dayName) {
