cab6c4969cc33805857d3238271477be4a799fc7
Saam Koukpari <saam.koukpari@gmail.com>
2021-10-31 13:10:56 -0700

    created custom hooks with associated tests

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+ 
+
+export default function useVisualMode(initial){
+  const [history, setHistory] = useState([initial]); 
+
+  function transition(newMode, replace = false) {
+    setHistory((prev) => {
+      const newHistory =[...prev];
+      if (replace) {
+        newHistory.pop();
+      }
+      return [...newHistory, newMode];
+    });
+  };
+
+  function back() {
+    if (history.length < 2) {
+      return;
+    }
+    setHistory((prev) => [...prev.slice(0, history.length - 1)]);
+  };
+
+  const mode = history[history.length - 1];
+  return {mode, transition, back };
+} 
